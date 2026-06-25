c24d7b434f90f67cf447a5e9af7bc3b437776335
Enock <kipkorirenock594@gmail.com>
2026-06-25 23:41:43 +0300

    stringFormatter in javascript.

---
+++ app/stringFormatter.js
@@ -0,0 +1,15 @@
+function cleanText(text){
+    while (text.first() === " "){
+        text.first().remove
+    }
+
+}
+function capitalize(text){
+    return text.first().upper()
+
+}
+function formatDisplayName(firstName, lastName){
+    let formatted = cleanText(firstName)
+
+    return `${firstName}`
+}
